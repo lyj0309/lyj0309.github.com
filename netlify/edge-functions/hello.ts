586011a7ad3089dcae986e519176bf0626da8f25
lyj <2457395722@qq.com>
2023-02-15 07:22:54 +0000

    update

---
+++ netlify/edge-functions/hello.ts
@@ -0,0 +1,184 @@
+/**
+ * static files (404.html, sw.js, conf.js)
+ */
+const ASSET_URL = 'https://www.fakev.cn'
+// 前缀，如果自定义路由为example.com/gh/*，将PREFIX改为 '/gh/'，注意，少一个杠都会错！
+const PREFIX = '/gh/'
+// 分支文件使用jsDelivr镜像的开关，0为关闭，默认关闭
+const Config = {
+    jsdelivr: 0
+}
+
+const whiteList = [] // 白名单，路径里面有包含字符的才会通过，e.g. ['/username/']
+
+/** @type {ResponseInit} */
+const PREFLIGHT_INIT: ResponseInit = {
+    status: 204,
+    headers: new Headers({
+        'access-control-allow-origin': '*',
+        'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
+        'access-control-max-age': '1728000',
+    }),
+}
+
+
+const exp1 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:releases|archive)\/.*$/i
+const exp2 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:blob|raw)\/.*$/i
+const exp3 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:info|git-).*$/i
+const exp4 = /^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+?\/.+$/i
+const exp5 = /^(?:https?:\/\/)?gist\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+$/i
+const exp6 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/tags.*$/i
+
+function makeRes(body: any, status: number = 200, headers: { [s: string]: string } = {}) {
+    headers['access-control-allow-origin'] = '*'
+    return new Response(body, {status, headers})
+}
+
+
+function newUrl(urlStr: string) {
+    try {
+        return new URL(urlStr)
+    } catch (err) {
+        return null
+    }
+}
+
+
+
+function checkUrl(u) {
+    for (let i of [exp1, exp2, exp3, exp4, exp5, exp6]) {
+        if (u.search(i) === 0) {
+            return true
+        }
+    }
+    return false
+}
+
+async function handler(req:Request):Promise<Response> {
+    const urlStr = req.url
+    const urlObj = new URL(urlStr)
+    let path = urlObj.searchParams.get('q')
+    if (path) {
+        return Response.redirect('https://' + urlObj.host + PREFIX + path, 301)
+    }
+    // cfworker 会把路径中的 `//` 合并成 `/`
+    path = urlObj.href.substring(urlObj.origin.length + PREFIX.length).replace(/^https?:\/+/, 'https://')
+    if (path.search(exp1) === 0 || path.search(exp5) === 0 || path.search(exp6) === 0 || path.search(exp3) === 0 || path.search(exp4) === 0) {
+        return httpHandler(req, path)
+    } else if (path.search(exp2) === 0) {
+        if (Config.jsdelivr) {
+            const newUrl = path.replace('/blob/', '@').replace(/^(?:https?:\/\/)?github\.com/, 'https://cdn.jsdelivr.net/gh')
+            return Response.redirect(newUrl, 302)
+        } else {
+            path = path.replace('/blob/', '/raw/')
+            return httpHandler(req, path)
+        }
+    } else if (path.search(exp4) === 0) {
+        const newUrl = path.replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, '@$1').replace(/^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/, 'https://cdn.jsdelivr.net/gh')
+        return Response.redirect(newUrl, 302)
+    } else {
+        // return fetch(ASSET_URL + path)
+        return fetch(ASSET_URL)
+    }
+}
+
+
+function httpHandler(req: Request, pathname: string) {
+    const reqHdrRaw = req.headers
+
+    // preflight
+    if (req.method === 'OPTIONS' &&
+        reqHdrRaw.has('access-control-request-headers')
+    ) {
+        return new Response(null, PREFLIGHT_INIT)
+    }
+
+    const reqHdrNew = new Headers(reqHdrRaw)
+
+    let urlStr = pathname
+    let flag = !Boolean(whiteList.length)
+    for (let i of whiteList) {
+        if (urlStr.includes(i)) {
+            flag = true
+            break
+        }
+    }
+    if (!flag) {
+        return new Response("blocked", {status: 403})
+    }
+    if (urlStr.startsWith('github')) {
+        urlStr = 'https://' + urlStr
+    }
+    const urlObj = newUrl(urlStr)
+
+    const reqInit: RequestInit = {
+        method: req.method,
+        headers: reqHdrNew,
+        redirect: 'manual',
+        body: req.body
+    }
+    if (urlObj == null){
+        return new Response("url empty", )
+    }
+    return proxy(urlObj, reqInit)
+}
+
+
+async function proxy(urlObj: URL|null, reqInit: RequestInit) {
+
+    if(urlObj == null){
+        return
+    }
+    const res = await fetch(urlObj.href, reqInit)
+    const resHdrOld = res.headers
+    const resHdrNew = new Headers(resHdrOld)
+
+    const status = res.status
+
+    if (resHdrNew.has('location')) {
+        let _location = resHdrNew.get('location')
+        if (checkUrl(_location))
+            resHdrNew.set('location', PREFIX + _location)
+        else {
+            reqInit.redirect = 'follow'
+            return proxy(newUrl(_location??""), reqInit)
+        }
+    }
+    resHdrNew.set('access-control-expose-headers', '*')
+    resHdrNew.set('access-control-allow-origin', '*')
+
+    resHdrNew.delete('content-security-policy')
+    resHdrNew.delete('content-security-policy-report-only')
+    resHdrNew.delete('clear-site-data')
+
+    return new Response(res.body, {
+        status,
+        headers: resHdrNew,
+    })
+}
+
+export default async (request: Request):Promise<Response> => {
+    const ret = handler(request)
+        .catch(err => makeRes('cfworker error:\n' + err.stack, 502))
+    return ret;
+
+    const resp = await fetch("https://api.github.com/users/denoland", {
+        // The init object here has an headers object containing a
+        // header that indicates what type of response we accept.
+        // We're not specifying the method field since by default
+        // fetch makes a GET request.
+        headers: {
+          accept: "application/json",
+        },
+      });
+      return new Response(resp.body, {
+        status: resp.status,
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+    return new Response("Hello, World!", {
+      headers: { "content-type": "text/html" },
+    });
+  };
+export const config = { path: "/gh**" }
